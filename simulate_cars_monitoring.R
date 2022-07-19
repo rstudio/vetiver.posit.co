9a77c3ec1253ad740f5b19d38f3773f201a8cb83
Julia Silge <julia.silge@gmail.com>
2022-07-19 17:22:16 -0600

    Update monitoring cars dataset for less confusing plots, etc

--- simulate_cars_monitoring.R
+++ simulate_cars_monitoring.R
@@ -9,5 +9,5 @@ new_mtcars <- sim_df(mtcars, n = 50) %>%
 
 new_mtcars %>%
     select(-id) %>%
-    mutate(date_obs = as.Date("2022-03-14") + 1:50) %>%
+    mutate(date_obs = as.Date("2022-03-20") + 0:49) %>%
     write_csv("get-started/new-cars.csv")
