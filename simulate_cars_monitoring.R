31711c69aefc649cca93e68da95742e695c0997a
Julia Silge <julia.silge@gmail.com>
2022-07-19 17:28:13 -0600

    Try yet again with simulated cars

--- simulate_cars_monitoring.R
+++ simulate_cars_monitoring.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(faux)
 
 set.seed(123)
-new_mtcars <- sim_df(mtcars, n = 50) %>%
+new_mtcars <- sim_df(mtcars, n = 60) %>%
     mutate(across(c(cyl, vs, am, gear, carb), round),
            across(c(cyl, vs, am, gear, carb), abs))
 
 
 new_mtcars %>%
     select(-id) %>%
-    mutate(date_obs = as.Date("2022-03-23") + 1:50) %>%
+    mutate(date_obs = as.Date("2022-03-23") + 1:60) %>%
     write_csv("get-started/new-cars.csv")
